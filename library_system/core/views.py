11fe5da034d390b9e4d9c202b03ecd30e225abf0
Shubhrajit Karan <sjkaran05@gmail.com>
2026-03-19 10:57:34 +0530

    adding db

--- library_system/core/views.py
+++ library_system/core/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from .models import Book, Transaction
 
 def login_view(request):
     return render(request, 'index.html')
 
 def librarian_dashboard(request):
-    return render(request, 'librarian.html')
+    # Count the real numbers from the database
+    total_books = Book.objects.count() # Counts unique book titles
+    active_checkouts = Transaction.objects.filter(status='ACTIVE').count()
+    overdue_books = Transaction.objects.filter(status='OVERDUE').count()
+
+    # Package the data to send to the HTML
+    context = {
+        'total_books': total_books,
+        'active_checkouts': active_checkouts,
+        'overdue_books': overdue_books,
+    }
+    return render(request, 'librarian.html', context)
 
 def student_portal(request):
-    return render(request, 'student.html')
+    # Fetch all books from the database
+    all_books = Book.objects.all()
+    
+    context = {
+        'books': all_books
+    }
+    return render(request, 'student.html', context)
